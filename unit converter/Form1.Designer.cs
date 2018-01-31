6b124745c223f2f9bffdb3950e6e94f63fadff06
Brown <rc0029451@student.riversidecollege.ac.uk>
2018-01-31 13:23:09 +0000

    Add project files.

---
+++ unit converter/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace unit_converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(130, 183);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(451, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(532, 183);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 2;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(285, 183);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 21);
+            this.comboBox4.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(872, 482);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+    }
+}
+
---
+++ unit converter/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace unit_converter
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, double> prefixes = new Dictionary<string, double>()
+ {
+ {"nm", 1e-9 },
+ {"μm", 1e-6 },
+ {"mm", 1e-3 },
+ {"cm", 1e-2 },
+ {"m", 1 },
+ {"km", 1e3 },
+ {"Mm", 1e6 },
+ {"Gm", 1e9 }
+ };
+        public Form1()
+        {
+            InitializeComponent();
+            foreach (string k in prefixes.Keys)
+            {
+                comboBox1.Items.Add(k);
+                comboBox2.Items.Add(k);
+            }
+            comboBox1.SelectedIndex = 4;
+            comboBox2.SelectedIndex = 4;
+            update();        }
+        void update()
+        {
+            try
+            {
+                double d = double.Parse(textBox1.Text);
+                double d2 = d * prefixes[comboBox1.Text] / prefixes[comboBox2.Text];
+                label1.Text = d2.ToString();
+            }
+            catch
+            {
+                label1.Text = "";
+
+
+          private void label1Click(object sender, EventArgs e)
+        {
+            update();
+        }
+
+          private void Form1_Load(object sender, EventArgs e)
+        {
+            update(); 
+        }
+    }
+}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
